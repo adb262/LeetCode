131d96fe1a816cfe57f1c7a57925906fd075e39b
adb262 <adb262@cornell.edu>
2020-08-28 14:54:23 -0400

    3

--- 8_24_2020.py
+++ 8_24_2020.py
@@ -41,3 +41,60 @@ def oddEvenList(self, head: ListNode) -> ListNode:
     llO.next = original
 
     return originalO
+
+
+def searchRange(nums, target):
+    """
+    Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+    Your algorithm's runtime complexity must be in the order of O(log n).
+
+    If the target is not found in the array, return [-1, -1].
+    """
+    if not nums:
+        return [-1, -1]
+    pointer = len(nums) // 2
+    res = set()
+
+    def rightSearch(nums, target):
+        index = 0
+        result = []
+        while index < len(nums):
+            if nums[index] > target:
+                break
+            if nums[index] == target:
+                result.append(index)
+            index += 1
+        return result
+
+    def leftSearch(nums, target, index):
+        result = []
+        while index >= 0:
+            if nums[index] < target:
+                break
+            if nums[index] == target:
+                result.insert(0, index)
+            index -= 1
+        return result
+
+    resRight = list(map(lambda x: x + pointer + 1,
+                        rightSearch(nums[pointer+1:], target)))
+    resLeft = leftSearch(nums[:pointer+1], target, pointer)
+    res = resLeft + resRight
+    return [res[0], res[-1]] if res else [-1, -1]
+
+
+def firstBadVersion(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    left = 1
+    right = n
+    while left < right:
+        mid = left + ((right - left) // 2)
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
